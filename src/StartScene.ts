e81f5a7f4f7390ffbda781f956afeddc6e42f19f
Marvin Altemeier <marv.internetz@gmail.com>
2019-10-11 21:03:58 +0200

    Add start screen

---
+++ src/StartScene.ts
@@ -0,0 +1,23 @@
+import { Scene } from 'phaser';
+import { screenWidth, screenHeight } from './config';
+import { Button } from './Button';
+
+export class StartScene extends Scene {
+    private _startButton: Button;
+
+    constructor() {
+        super({ key: 'StartScene' });
+    }
+
+    public create(): void {
+        this._startButton = new Button(
+            this,
+            screenWidth / 2,
+            screenHeight / 2,
+            'Start',
+            '#FFF',
+            '#D50C2D',
+            () => this.scene.start('TestScene')
+        );
+    }
+}
