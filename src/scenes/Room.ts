a5cdc3a04f80fc2bf500c272247fc73443069559
Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>
2019-10-11 23:11:48 +0200

    fix player
    Signed-off-by: Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>

--- src/scenes/Room.ts
+++ src/scenes/Room.ts
@@ -1,25 +1,33 @@
 import { Scene } from 'phaser';
 import { Score } from '../objects/Score';
+import { Player } from '../Player';
 
 export class Room extends Scene {
     private _score: Score;
+    private _player: Player;
     constructor() {
         super({ key: 'Room' });
     }
 
     public preload() {
         this.load.image('background', 'assets/backgrounds/room.png');
+        this.load.spritesheet('player', 'assets/player.png', {
+            frameWidth: 64,
+            frameHeight: 64,
+        });
     }
 
     public create() {
-        this._score = new Score(this);
-        this.physics.world.setBounds(0, 0, 800, 600);
-
         const background = this.add.image(0, 0, 'background');
         background.setOrigin(0, 0).setDisplaySize(800, 600);
+
+        this._score = new Score(this);
+        this._player = new Player(this);
+        this.physics.world.setBounds(0, 0, 800, 600);
     }
 
     public update(): void {
         this._score.update();
+        this._player.update();
     }
 }
