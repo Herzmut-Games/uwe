de5603d948836d16bac1b0e51a62101bdc9373b1
Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>
2019-10-12 15:36:51 +0200

    improve menu
    Signed-off-by: Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>

--- src/scenes/Start.ts
+++ src/scenes/Start.ts
@@ -15,6 +15,10 @@ export class Start extends Scene {
             'menu-select',
             'assets/sounds/effects/sfx_menu_select1.wav'
         );
+        this.load.spritesheet('player', 'assets/georg.png', {
+            frameWidth: 48,
+            frameHeight: 48,
+        });
     }
 
     public destroy(): void {
@@ -25,58 +29,51 @@ export class Start extends Scene {
         this._music = this.sound.add('intro', { volume: 0.5, loop: true });
         this._music.play();
 
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', {
+                frames: [0, 4, 8, 12],
+            }),
+            frameRate: 5.2,
+            repeat: -1,
+        });
+
+        const menuPlayer = this.physics.add.sprite(200, 350, 'player');
+        menuPlayer.setOrigin(0.5, 0.5);
+        menuPlayer.setScale(7);
+        menuPlayer.play('down');
+
         this.add
-            .text(screenWidth / 2, screenHeight / 4, 'UWE', {
+            .text(screenWidth / 2, 80, 'UWE', {
                 fill: '#D50C2D',
                 fontSize: 100,
                 fontFamily: fonts.primary,
             })
             .setOrigin(0.5, 0.5);
         this.add
-            .text(
-                screenWidth / 2,
-                screenHeight / 3,
-                '(Das E steht für Elemente)',
-                {
-                    fill: '#D50C2D',
-                    fontSize: 25,
-                    fontFamily: fonts.primary,
-                }
-            )
+            .text(screenWidth / 2, 130, '(Das E steht für Elemente)', {
+                fill: '#D50C2D',
+                fontSize: 25,
+                fontFamily: fonts.primary,
+            })
             .setOrigin(0.5, 0.5);
         this.add
-            .text(screenWidth / 1.5, screenHeight / 5, 'Bist du auch dabei?', {
+            .text(screenWidth / 1.5, 60, 'Bist du auch dabei?', {
                 fill: '#FFFF00',
                 fontSize: 20,
                 fontFamily: fonts.primary,
             })
             .setOrigin(0.5, 0.5)
             .setAngle(30);
-        Button.create(
-            this,
-            screenWidth / 2,
-            screenHeight / 2,
-            'Start',
-            '#FFF',
-            '#D50C2D',
-            () => {
-                this.scene.start('Room');
-                this.sound.add('menu-select').play();
-                this.destroy();
-            }
-        );
+        Button.create(this, 550, 350, 'Start', '#FFF', '#D50C2D', () => {
+            this.scene.start('Room');
+            this.sound.add('menu-select').play();
+            this.destroy();
+        });
 
-        Button.create(
-            this,
-            screenWidth / 2,
-            screenHeight / 2 + 50,
-            'DIE',
-            '#D50C2D',
-            '#FFF',
-            () => {
-                this.scene.start('Death', { score: 1337 });
-                this.destroy();
-            }
-        );
+        Button.create(this, 550, 430, 'About', '#D50C2D', '#FFF', () => {
+            this.scene.start('Death', { score: 1337 });
+            this.destroy();
+        });
     }
 }
