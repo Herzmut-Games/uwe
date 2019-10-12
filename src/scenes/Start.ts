62bff01ef8cd4e4f2cc0673294ff1b768a241b37
Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>
2019-10-12 22:25:57 +0200

    improve thriller
    Signed-off-by: Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>

--- src/scenes/Start.ts
+++ src/scenes/Start.ts
@@ -4,6 +4,7 @@ import { Button } from '../objects/Button';
 import { fonts } from '../objects/Fonts';
 import { SingleEntryPlugin } from 'webpack';
 import { Room } from './Room';
+import { tsExpressionWithTypeArguments } from '@babel/types';
 
 export class Start extends Scene {
     private _music: Phaser.Sound.BaseSound;
@@ -11,17 +12,23 @@ export class Start extends Scene {
     private _menuPlayer: Phaser.Physics.Arcade.Sprite;
     private _playerRunAway: boolean;
     private _runAwayModifier: number = 0;
+    private _backgroundModifier: number = 2.75;
     private _startButton: Button;
     private _aboutButton: Button;
     private _aboutText: Phaser.GameObjects.Text;
     private _aboutBackButton: Button;
     private _moonWalkEnabled: boolean = false;
+    private _background_dark: Phaser.GameObjects.TileSprite;
     constructor() {
         super({ key: 'Start' });
     }
 
     public preload() {
         this.load.image('menu-background', 'assets/backgrounds/menu.png');
+        this.load.image(
+            'menu-background-dark',
+            'assets/backgrounds/menu_dark.png'
+        );
         this.load.audio('intro', 'assets/sounds/intro.mp3');
         this.load.audio('thriller', 'assets/sounds/thriller.mp3');
         this.load.audio(
@@ -42,6 +49,16 @@ export class Start extends Scene {
         this._background = this.add.tileSprite(100, 0, 0, 0, 'menu-background');
         this._background.setScale(1.2);
 
+        this._background_dark = this.add.tileSprite(
+            100,
+            0,
+            0,
+            0,
+            'menu-background-dark'
+        );
+        this._background_dark.setScale(1.2);
+        this._background_dark.setAlpha(0);
+
         this._music = this.sound.add('intro', { volume: 0.5, loop: true });
         this._music.play();
 
@@ -57,13 +74,31 @@ export class Start extends Scene {
             this._aboutButton.setAlpha(1 - this._runAwayModifier * 0.1);
         }
         this._menuPlayer.x += this._runAwayModifier;
-        this._background.tilePositionX += 2.75 + this._runAwayModifier;
+        this._background.tilePositionX +=
+            this._backgroundModifier + this._runAwayModifier;
+        this._background_dark.tilePositionX +=
+            this._backgroundModifier + this._runAwayModifier;
 
         if (this._menuPlayer.x >= 900) {
             this._playerRunAway = false;
             this._runAwayModifier = 0;
             this.scene.start('Room');
             this.destroy();
+        }
+
+        console.log(this._background_dark.alpha);
+        if (this._moonWalkEnabled) {
+            if (this._background_dark.alpha < 1) {
+                this._background_dark.setAlpha(
+                    this._background_dark.alpha + 0.03
+                );
+            }
+        } else {
+            if (this._background_dark.alpha > 0) {
+                this._background_dark.setAlpha(
+                    this._background_dark.alpha - 0.1
+                );
+            }
         }
     }
 
@@ -73,7 +108,7 @@ export class Start extends Scene {
             frames: this.anims.generateFrameNumbers('player', {
                 frames: [1, 5, 9, 13],
             }),
-            frameRate: 5.2,
+            frameRate: 4,
             repeat: -1,
         });
         this.anims.create({
@@ -83,6 +118,14 @@ export class Start extends Scene {
             }),
             frameRate: 5.2,
             repeat: -1,
+        });
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', {
+                frames: [0],
+            }),
+            frameRate: 5.2,
+            repeat: 0,
         });
         this.anims.create({
             key: 'right-fast',
@@ -104,21 +147,38 @@ export class Start extends Scene {
     private _toggleMoonWalk(): void {
         this._moonWalkEnabled = !this._moonWalkEnabled;
         if (this._moonWalkEnabled) {
-            this._music.stop();
-            this._music = this.sound.add('thriller', { loop: true });
-            this._music.play();
-            this.time.delayedCall(
-                934,
-                () => this._menuPlayer.anims.play('left'),
-                [],
-                this
-            );
+            this._enableMoonwalk();
         } else {
-            this._menuPlayer.anims.play('right');
-            this._music.stop();
-            this._music = this.sound.add('intro', { volume: 0.5, loop: true });
-            this._music.play();
+            this._disableMoonwalk();
         }
+    }
+
+    private _enableMoonwalk(): void {
+        this._moonWalkEnabled = true;
+        this._music.stop();
+        this._music = this.sound.add('thriller', { loop: true });
+        this._menuPlayer.anims.play('down');
+        this._backgroundModifier = 0;
+        this._music.play();
+        this.time.delayedCall(
+            934,
+            () => {
+                if (this._moonWalkEnabled) {
+                    this._menuPlayer.anims.play('left');
+                }
+                this._backgroundModifier = 2.75;
+            },
+            [],
+            this
+        );
+    }
+
+    private _disableMoonwalk(): void {
+        this._moonWalkEnabled = false;
+        this._menuPlayer.anims.play('right');
+        this._music.stop();
+        this._music = this.sound.add('intro', { volume: 0.5, loop: true });
+        this._music.play();
     }
 
     private _displayHeader(): void {
@@ -189,6 +249,7 @@ export class Start extends Scene {
             '#D50C2D',
             '58px',
             () => {
+                this._disableMoonwalk();
                 this.sound.add('menu-select').play();
                 this._menuPlayer.anims.play('right-fast');
                 this._playerRunAway = true;
