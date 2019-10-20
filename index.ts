d1fa4ec4af28a3d53f6cbb95dc33ea04c70571ee
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2019-10-20 18:05:02 +0200

    Extend game class

--- index.ts
+++ index.ts
@@ -1,43 +1,6 @@
-import { screenWidth, screenHeight } from './src/configs/Screen';
-import { Game, Types } from 'phaser';
-import { Menu } from './src/scenes/menu/Menu';
-import { Room } from './src/scenes/room/Room';
-import { Death } from './src/scenes/Death';
-import { Intro } from './src/scenes/Intro';
-import { Help } from './src/scenes/Help';
-import { About } from './src/scenes/menu/About';
-import { Selection } from './src/scenes/menu/Selection';
-import { TopBar } from './src/scenes/room/TopBar';
-import { CountDown } from './src/scenes/room/CountDown';
+import { UweGame } from './src/Game';
 
-export const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: screenWidth,
-    height: screenHeight,
-    parent: document.getElementById('game'),
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-        },
-    },
-    scene: [
-        Intro,
-        About,
-        Selection,
-        Menu,
-        Death,
-        TopBar,
-        CountDown,
-        Room,
-        Help,
-    ],
-    render: {
-        pixelArt: true,
-    },
-    audio: {
-        disableWebAudio: true,
-    },
+window.onload = () => {
+    // tslint:disable-next-line: no-unused-expression
+    new UweGame();
 };
-
-const game = new Game(config);
---
+++ src/Game.ts
@@ -0,0 +1,50 @@
+import { Game, Types } from 'phaser';
+import { screenWidth, screenHeight } from './configs/Screen';
+import { Intro } from './scenes/Intro';
+import { About } from './scenes/menu/About';
+import { Menu } from './scenes/menu/Menu';
+import { Death } from './scenes/Death';
+import { TopBar } from './scenes/room/TopBar';
+import { CountDown } from './scenes/room/CountDown';
+import { Room } from './scenes/room/Room';
+import { Help } from './scenes/Help';
+import { Selection } from './scenes/menu/Selection';
+
+const parent: string = 'game';
+
+export class UweGame extends Game {
+    constructor() {
+        const config: Types.Core.GameConfig = {
+            type: Phaser.AUTO,
+            width: screenWidth,
+            height: screenHeight,
+            parent,
+            physics: {
+                default: 'arcade',
+            },
+            fps: {
+                min: 40,
+                target: 40,
+            },
+            scene: [
+                Intro,
+                About,
+                Selection,
+                Menu,
+                Death,
+                TopBar,
+                CountDown,
+                Room,
+                Help,
+            ],
+            render: {
+                pixelArt: true,
+            },
+            audio: {
+                disableWebAudio: true,
+            },
+        };
+
+        super(config);
+    }
+}
